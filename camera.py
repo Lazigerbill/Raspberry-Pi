fdfb088536ea65bba4acf926046a76fb36ef9f21
Lazigerbill <chaukbill@gmail.com>
2015-04-27 00:19:50 -0400

    debug brightness

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ with picamera.PiCamera() as camera:
 	camera.start_preview()
 	try:
 		for i in range(100):
-			camera.brigtness = i
+			camera.brightness = i
 			time.sleep(0.2)
 	finally:
 		camera.stop_preview()
