1966229cb5b18b27243c347a4219a96a0bf959ce
Lazigerbill <chaukbill@gmail.com>
2015-04-27 03:48:54 +0000

    This is a new repo for my ResPi

---
+++ storytime.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+print("Hello " + name)
+gender = input("Are you a boy or a girl?")
+if gender == "boy":kj  mjkvhcn
+    pronoun = "it"
+print(pronoun + " is ugly")
