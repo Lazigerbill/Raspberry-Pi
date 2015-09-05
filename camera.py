f7f236da370f1a5905b2e8a1f4f2a3e981c07a34
Lazigerbill <chaukbill@gmail.com>
2015-09-05 13:17:25 +0000

    restarting project

--- camera.py
+++ camera.py
@@ -2,13 +2,14 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    # camera.resolution = (1280, 720)
+    camera.resolution = (1280, 720)
     camera.start_preview()
-    # camera.exposure_compensation = 2
+    camera.xposure_compensation = 2
     # camera.exposure_mode = 'spotlight'
-    # camera.meter_mode = 'matrix'
-    # camera.image_effect = 'gpen'
+    camera.meter_mode = 'matrix'
+    camera.vflip = 'true'
+    # camera.image_effect = 'film'
     # Give the camera some time to adjust to conditions
-    time.sleep(20)
+    time.sleep(30)
     # camera.capture('foo.jpg')
     camera.stop_preview()
