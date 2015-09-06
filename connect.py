98fed31a14c313f4108a8cbb98eefd3d5de19fdd
Lazigerbill <chaukbill@gmail.com>
2015-09-06 15:22:04 -0400

    adding ultrasonic

--- connect.py
+++ connect.py
@@ -1,21 +1,22 @@
 import ibmiotf.device
 import time
+import grovepi
 
 try:
-	options = {
-	"org": "z4cnep",
-	"type": "RaspberryPi",
-	"id": "000000001bb887a7",
-	"auth-method": "token",
-	"auth-token": "6xzR?XvuXGVd2+k?qq"
-	}
+	options = ibmiotf.device.ParseConfigFile("/Desktop/IoT/device.cfg")
 	client = ibmiotf.device.Client(options)
 	client.connect()
 
+	ultrasonic_ranger = 4
 	while True:
-		myData={'name' : 'foo', 'cpu' : 60, 'mem' : 50}
-		client.publishEvent(event="status", msgFormat="json", data=myData)
-		time.sleep(1)
+		try:
+		    # Read distance value from Ultrasonic
+		    reading = grovepi.ultrasonicRead(ultrasonic_ranger)
+		except:
+		    print ("Error reading ultrasonic")
+		myData={'distance' : reading}
+		client.publishEvent("status", "json", myData)
+		time.sleep(10)
 
 except ibmiotf.ConnectionException  as e:
-		print("error")
+	print(e)
