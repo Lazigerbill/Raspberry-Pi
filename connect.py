caa6c32a92719247ed561a1d58dad6e2b70ef61d
Lazigerbill <chaukbill@gmail.com>
2015-09-06 14:07:59 -0400

    added while True

--- connect.py
+++ connect.py
@@ -1,4 +1,5 @@
 import ibmiotf.device
+import time
 
 try:
   options = {
@@ -9,9 +10,12 @@ try:
     "auth-token": "6xzR?XvuXGVd2+k?qq"
   }
   client = ibmiotf.device.Client(options)
+	client.connect()
+
+	while True:
+		myData={'name' : 'foo', 'cpu' : 60, 'mem' : 50}
+		client.publishEvent(event="status", msgFormat="json", data=myData)
+		time.sleep(1)
+
 except ibmiotf.ConnectionException  as e:
 	print("error")
-
-client.connect()
-myData={'name' : 'foo', 'cpu' : 60, 'mem' : 50}
-client.publishEvent(event="status", msgFormat="json", data=myData)
