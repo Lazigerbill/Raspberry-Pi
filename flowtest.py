3bba03a9159fe83ea3ddbcbb6af425a280edb7a0
Lazigerbill <chaukbill@gmail.com>
2015-09-12 13:20:51 -0400

    fixed indentation issue

--- flowtest.py
+++ flowtest.py
@@ -14,35 +14,35 @@ def cpu_temp():
 	output, error = process.communicate()
 	return float(ooutput[output.index('=') + 1:output.rindiex("'")])
 
-	def drop():
-		drop = {
-			"elems": {
-				"cpu_temp": {
-				"type": "float",
-				"value": cpu_temp()
-				},
-				"ram": {
-				"type": "map",
-				"value": {}
-				},
-				"disk": {
-				"type": "map",
-				"value": {}
-				}
-			}	
-		}
+def drop():
+	drop = {
+		"elems": {
+			"cpu_temp": {
+			"type": "float",
+			"value": cpu_temp()
+			},
+			"ram": {
+			"type": "map",
+			"value": {}
+			},
+			"disk": {
+			"type": "map",
+			"value": {}
+			}
+		}	
+	}
 
-		ram = psutil.phymem_usage()
-		drop['elems']['ram']['value']['total'] = ram.total / 2 ** 20
-		drop['elems']['ram']['value']['used'] = ram.used / 2 ** 20
-		drop['elems']['ram']['value']['free'] = ram.free / 2 ** 20
+	ram = psutil.phymem_usage()
+	drop['elems']['ram']['value']['total'] = ram.total / 2 ** 20
+	drop['elems']['ram']['value']['used'] = ram.used / 2 ** 20
+	drop['elems']['ram']['value']['free'] = ram.free / 2 ** 20
 
-		disk = psutil.disk_usage('/')
-		drop['elems']['disk']['value']['total'] = disk.total / 2 ** 20
-		drop['elems']['disk']['value']['used'] = disk.used / 2 ** 20
-		drop['elems']['disk']['value']['free'] = disk.free / 2 ** 20
+	disk = psutil.disk_usage('/')
+	drop['elems']['disk']['value']['total'] = disk.total / 2 ** 20
+	drop['elems']['disk']['value']['used'] = disk.used / 2 ** 20
+	drop['elems']['disk']['value']['free'] = disk.free / 2 ** 20
 
-		return drop
+	return drop
 
  # setup your credentials
 creds = Token(ACCOUNT_NAME, ACCOUNT_TOKEN)
