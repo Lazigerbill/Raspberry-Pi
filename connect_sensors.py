88b7fbfdffb7ff5a68e49001c1010f36094dcbc7
Lazigerbill <chaukbill@gmail.com>
2015-09-28 23:17:10 -0400

    cleaned up code for sensors

---
+++ connect_sensors.py
@@ -0,0 +1,52 @@
+import time
+from datetime import datetime
+import ibmiotf.device
+from grovepi import grovepi
+
+try:
+	options = ibmiotf.device.ParseConfigFile('/home/pi/Desktop/IoT/sensors.cfg')
+	client = ibmiotf.device.Client(options)
+	client.connect()
+	
+	while True:
+		# define pin number for sensors here
+		dht = 4
+		light_sensor = 1
+		sound_sensor = 0
+		try:
+			temp = grovepi.dht(dht,0)[0]
+		except: 
+			temp = None
+		try:
+			humidity = grovepi.dht(dht,0)[1]
+		except:
+			humidity = None
+		try:
+			lightlvl = grovepi.analogRead(light_sensor)
+		except:
+			lightlvl = None
+		try:
+			soundlvl = grovepi.analogRead(sound_sensor)
+		except:
+			soundlvl = None
+
+		myQosLevel=1
+
+		# timestamp is UTC, and UTC time shall be used across
+		myData={"d": {'temp':temp, 'humidity':humidity, 'lightlvl':lightlvl, 'soundlvl':soundlvl}, "ts": datetime.utcnow().isoformat()+"Z"}
+		client.publishEvent("IC306A", "json", myData, myQosLevel)
+		time.sleep(30)
+
+except ibmiotf.ConnectionException as e:
+	print(e)
+
+# def myCommandCallback(cmd):
+#   print("Command received: %s" % cmd.data)
+#   if cmd.command == "TurnOn":
+#     grovepi.digitalWrite(2,1)
+#     print("LED is now turned ON")
+#   elif cmd.command == "TurnOff":
+#     grovepi.digitalWrite(2,0)
+#     print("LED is now turned ON")
+
+
