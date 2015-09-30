7514b2d3ab2ede343c9ee9a8f00d0cf833c1f116
Lazigerbill <chaukbill@gmail.com>
2015-09-29 21:34:02 -0400

    updated camera code

--- connect_camera.py
+++ connect_camera.py
@@ -1,23 +1,59 @@
 import time
+from datetime import datetime
+import math
+import picamera
+import json
+import base64
 import ibmiotf.device
+import random, string
+
+def randomword(length):
+  return ''.join(random.choice(string.lowercase) for i in range(length))
+
+def convertImageToBase64():
+	with open("live_pic.jpg", "rb") as image_file:
+		encoded = base64.b64encode(image_file.read())
+	return encoded
+
+def publishEncodedImage(encoded):
+	packet_size=3000
+	end = packet_size
+	start = 0
+	length = len(encoded)
+	picId = randomword(8)
+	pos = 0
+	no_of_packets = math.ceil(length/packet_size)
+
+	while start <= len(encoded):
+		data = {"d":{"data": encoded[start:end], "pic_id":picId, "pos":pos, "size":no_of_packets}, "ts": datetime.utcnow().isoformat()+"Z"}
+		client.publishEvent("Image-Data", "json", data, 0)
+		end += packet_size
+		start += packet_size
+		pos += 1
 
 try:
-	options = ibmiotf.device.ParseConfigFile('/Users/Bill/Desktop/IoT/Raspberry-Pi/device.cfg')
+	options = ibmiotf.device.ParseConfigFile('/home/pi/Desktop/IoT/camera.cfg')
 	client = ibmiotf.device.Client(options)
 	client.connect()
+	# client.commandCallback = myCommandCallback
+	
+	while True:
+		camera = picamera.PiCamera()
+		try:
+			camera.start_preview()
+			time.sleep(1)
+			camera.capture('live_pic.jpg', resize=(480, 320))
+			camera.stop_preview()
+			pass
+		finally:
+			camera.close()
 
-except ibmiotf.ConnectionException  as e:
+		publishEncodedImage(convertImageToBase64())
+
+		time.sleep(120)
+
+except ibmiotf.ConnectionException as e:
 	print(e)
-	exit()
-
-while True:
-	try:
-		myData={'bill' : 'testing', 'chau' : 'testing2'}
-	except:
-		myData={'bill':'N/A', 'chau':'N/A'}	
-	finally:
-		client.publishEvent("status", "json", myData)
-		time.sleep(300)
 
 
 
