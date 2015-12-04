2bf21ee15d2331f39d222ce1ca211955f08c1f55
Lazigerbill <chaukbill@gmail.com>
2015-12-04 22:29:01 +0000

    testing camera on pi

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
     camera.start_preview()
     camera.xposure_compensation = 2
-    # camera.exposure_mode = 'spotlight'
+    camera.exposure_mode = 'spotlight'
     camera.meter_mode = 'matrix'
     camera.vflip = 'true'
     # camera.image_effect = 'film'
