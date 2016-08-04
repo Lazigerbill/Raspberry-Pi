970b74e5967202c0a883aca4bf45ddb220557797
Lazigerbill <chaukbill@gmail.com>
2016-08-04 15:55:51 -0500

    fixed quotations

--- rs485.py
+++ rs485.py
@@ -1,17 +1,17 @@
 import serial
  
-port = ”/dev/ttyAMA0″
+port = "/dev/ttyAMA0"
 usart = serial.Serial(port,9600)
 usart.flushInput()
  
-print (“serial test: BaudRate = 9600″)
+print ("serial test: BaudRate = 9600")
  
-usart.write(“please enter the character:\r”)
+usart.write("please enter the character:\r")
  
 while True: 
    if( usart.inWaiting()>0 ) :
       receive = usart.read(1)
-      print ”receive: ”,receive
-      usart.write(“  send: ’”)
+      print "receive: ",receive
+      usart.write("send: '")
       usart.write(receive)
-      usart.write(“‘\r”)
+      usart.write("'\r")
