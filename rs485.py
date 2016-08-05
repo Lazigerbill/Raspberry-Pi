2320d77457ec7d7a2a5727e11d64e9f5b49778d3
Lazigerbill <chaukbill@gmail.com>
2016-08-05 17:12:19 -0400

    added try/except

--- rs485.py
+++ rs485.py
@@ -4,21 +4,24 @@ import minimalmodbus
 
 #Define port and slave address(decimal) here
 instr = minimalmodbus.Instrument("/dev/ttyAMA0", 1) 
+try:
+	while True:
+		try: 
+			fq = instr.read_float(int('4000',16)) 
+			v1 = instr.read_float(int('4002',16))
+			v12 = instr.read_float(int('400a',16))
+			current = instr.read_float(int('4012',16))
+			apower = instr.read_float(int('401c',16))
+		except Exception, e:
+			print str(e)
 
-while True:
-	try: 
-		fq = instr.read_float(int('4000',16)) 
-		v1 = instr.read_float(int('4002',16))
-		v12 = instr.read_float(int('400a',16))
-		current = instr.read_float(int('4012',16))
-		apower = instr.read_float(int('401c',16))
-	except Exception, e:
-		print str(e)
+		print "Frequency: %.2f" % fq
+		print "Phase Voltage: %.2f" % v1
+		print "Line Voltage: %.2f" % v12
+		print "Current: %.2f" % current
+		print "Phase A Power: %.2f" % apower
 
-	print "Frequency: %.2f" % fq
-	print "Phase Voltage: %.2f" % v1
-	print "Line Voltage: %.2f" % v12
-	print "Current: %.2f" % current
-	print "Phase A Power: %.2f" % apower
-
-	time.sleep(3000)
+		time.sleep(3000)
+		print ("complete...")
+except Exception, e:
+	print str(e)
