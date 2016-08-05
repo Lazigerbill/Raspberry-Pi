7421c7b58213fa852a5f2ebff771c2667150f978
Lazigerbill <chaukbill@gmail.com>
2016-08-05 16:50:00 -0400

    new code for powermeter

--- rs485.py
+++ rs485.py
@@ -1,17 +1,24 @@
-import serial
- 
-port = "/dev/ttyAMA0"
-usart = serial.Serial(port,9600)
-usart.flushInput()
- 
-print ("serial test: BaudRate = 9600")
- 
-usart.write("please enter the character:\r")
- 
-while True: 
-   if( usart.inWaiting()>0 ) :
-      receive = usart.read(1)
-      print "receive: ",receive
-      usart.write("send: '")
-      usart.write(receive)
-      usart.write("'\r")
+import time
+# import ibmiotf.device
+import minmalmodbus
+
+#Define port and slave address(decimal) here
+instr = minimalmodbus.Instrument("/dev/ttyAMA0", 1) 
+
+while True:
+	try: 
+		fq = instr.read_float(int('4000',16)) 
+		v1 = instr.read_float(int('4002',16))
+		v12 = instr.read_float(int('400a',16))
+		current = instr.read_float(int('4012',16))
+		apower = instr.read_float(int('401c',16))
+	except Exception, e:
+		print str(e)
+
+	print "Frequency: " + fq
+	print "Phase Voltage: " + v1
+	print "Line Voltage: " + v12
+	print "Current: " + current
+	print "Phase A Power: " + apower
+
+	time.sleep(3000)
