898beeae18e50d72dc298acf56a7fbf650aac07d
Lazigerbill <chaukbill@gmail.com>
2016-08-08 13:34:30 -0400

    debugged logging

--- rs485.py
+++ rs485.py
@@ -3,13 +3,13 @@ import datetime
 import ibmiotf.device
 import minimalmodbus
 import logging
-logging.basicConfig(filename='/home/pi/Rasberry-Pi/rs485.log',level=logging.DEBUG)
+logging.basicConfig(filename='/home/pi/Raspberry-Pi/rs485.log', filemode='w', level=logging.DEBUG)
 
 #initialize port
 try:
 	#Define port and slave address(decimal) here
 	instr = minimalmodbus.Instrument("/dev/ttyAMA0", 1)
-	logging.info("Port intialized, connected to /dev/ttyAMA0. ")
+	logging.info("Port intialized, connected successfully to /dev/ttyAMA0. ")
 except Exception, e:
 	logging.debug(str(e))
 
@@ -18,12 +18,14 @@ try:
 	options = ibmiotf.device.ParseConfigFile('/etc/rs485.cfg')
 	client = ibmiotf.device.Client(options)
 	client.connect()
-	myQosLevel=1
-	logging.info("IBM IoTF connected succesfully, QoS Level at '%s'") %(myQosLevel)
+	myQosLevel = 1
+	logging.info("IBM IoTF connected successfully, QoS Level at %i" % myQosLevel)
 except Exception, e:
 	logging.debug(str(e))
 	print str(e)
 
+#Loop starts here:
+#single phase power
 while True:
 	try: 
 		fq = instr.read_float(int('4000',16)) 
@@ -44,7 +46,7 @@ while True:
 		# print "Current: %.2f" % current
 		# print "Phase A Power: %.2f" % apower
 		print str(myData)
-		logging.info(str(myData))
+		# logging.info(str(myData))
 		time.sleep(5)
 
 	except Exception, e:
