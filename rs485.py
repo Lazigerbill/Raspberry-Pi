d66bd18abc6556acb03bb5e8c2edfbf54057f338
Lazigerbill <chaukbill@gmail.com>
2016-08-08 12:06:07 -0400

    logging added

--- rs485.py
+++ rs485.py
@@ -2,18 +2,26 @@ import time
 import datetime
 import ibmiotf.device
 import minimalmodbus
+import logging
+logging.basicConfig(filename='/home/pi/Rasberry-Pi/rs485.log',level=logging.DEBUG)
 
-#initiation
+#initialize port
 try:
 	#Define port and slave address(decimal) here
 	instr = minimalmodbus.Instrument("/dev/ttyAMA0", 1)
+	logging.info("Port intialized, connected to /dev/ttyAMA0. ")
+except Exception, e:
+	logging.debug(str(e))
 
-	#connect to IBM IoTF
+#connect to IBM IoTF
+try:
 	options = ibmiotf.device.ParseConfigFile('/etc/rs485.cfg')
 	client = ibmiotf.device.Client(options)
 	client.connect()
 	myQosLevel=1
+	logging.info("IBM IoTF connected succesfully, QoS Level at '%s'") %(myQosLevel)
 except Exception, e:
+	logging.debug(str(e))
 	print str(e)
 
 while True:
@@ -24,13 +32,21 @@ while True:
 		current = instr.read_float(int('4012',16))
 		apower = instr.read_float(int('401c',16))
 	except Exception, e:
+		logging.debug(str(e))
 		print str(e)
 
-	myData={'d': {'fq':fq, 'v1':v1, 'v12':v12, 'current':current, 'apower': apower}}
-	client.publishEvent("rs485", "json", myData, myQosLevel)	
-	print "Frequency: %.2f" % fq
-	print "Phase Voltage: %.2f" % v1
-	print "Line Voltage: %.2f" % v12
-	print "Current: %.2f" % current
-	print "Phase A Power: %.2f" % apower
-	time.sleep(5)
+	try: 
+		myData={'d': {'fq':fq, 'v1':v1, 'v12':v12, 'current':current, 'apower': apower}}
+		client.publishEvent("rs485", "json", myData, myQosLevel)	
+		# print "Frequency: %.2f" % fq
+		# print "Phase Voltage: %.2f" % v1
+		# print "Line Voltage: %.2f" % v12
+		# print "Current: %.2f" % current
+		# print "Phase A Power: %.2f" % apower
+		print str(myData)
+		logging.info(str(myData))
+		time.sleep(5)
+
+	except Exception, e:
+		logging.debug(str(e))
+		print str(e)
