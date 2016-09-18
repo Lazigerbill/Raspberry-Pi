69f314555e9532f1d53b64083c9fcaa204d84567
Lazigerbill <chaukbill@gmail.com>
2016-09-18 16:47:10 -0400

    back to working code

--- led_dimmer.py
+++ led_dimmer.py
@@ -5,17 +5,29 @@ import logging
 # logging.basicConfig(filename='/home/pi/Raspberry-Pi/rs485.log', filemode='w', level=logging.DEBUG)
 logging.basicConfig(filename='/home/pi/Documents/logs/led_dimmer.log', level=logging.DEBUG)
 
-# #Loop starts here:
-# while True:
-# 	myData={'txt': 'hello world', 'ts': datetime.datetime.utcnow().isoformat()+'Z'}
-# 	client.publishEvent("IC306A", "json", myData, myQosLevel)
-# 	time.sleep(5)
+#connect to IBM IoTF
+try:
+	options = ibmiotf.device.ParseConfigFile('/etc/rpi_iotf/ledpod.cfg')
+	client = ibmiotf.device.Client(options)
+	# myQosLevel = 1
+	client.connect()
+	# logging.info("IBM IoTF connected successfully, QoS Level at %i" % myQosLevel)
+	# client.commandCallback = myCommandCallback
+except ibmiotf.ConnectionException as e:
+	logging.debug(str(e))
+	print str(e)
+
+#Loop starts here:
+while True:
+	myData={'txt': 'hello world', 'ts': datetime.datetime.utcnow().isoformat()+'Z'}
+	client.publishEvent("IC306A", "json", myData, myQosLevel)
+	time.sleep(5)
 
 
 
 
-def myCommandCallback(cmd):
-	print("Command received: %s" % cmd.data)
+# def myCommandCallback(cmd):
+# 	print("Command received: %s" % cmd.data)
 	# if cmd.command == "setInterval":
 	# 	if 'interval' not in cmd.data:
 	# 		print("Error - command is missing required information: 'interval'")
@@ -26,14 +38,3 @@ def myCommandCallback(cmd):
 	# 			print("Error - command is missing required information: 'message'")
 	# 		else:
 	# 			print(cmd.data['message'])
-
-#connect to IBM IoTF
-try:
-	options = ibmiotf.device.ParseConfigFile('/etc/rpi_iotf/ledpod.cfg')
-	client = ibmiotf.device.Client(options)
-	# myQosLevel = 1
-except ibmiotf.ConnectionException as e:
-	logging.debug(str(e))
-	print str(e)
-client.connect()
-client.commandCallback = myCommandCallback
