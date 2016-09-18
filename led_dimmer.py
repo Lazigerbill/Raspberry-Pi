e7ca27f04337a5007d849e353b4b78f50f4a5728
Lazigerbill <chaukbill@gmail.com>
2016-09-18 16:13:28 -0400

    added commands to device

--- led_dimmer.py
+++ led_dimmer.py
@@ -9,10 +9,11 @@ logging.basicConfig(filename='/home/pi/Documents/logs/led_dimmer.log', level=log
 try:
 	options = ibmiotf.device.ParseConfigFile('/etc/rpi_iotf/ledpod.cfg')
 	client = ibmiotf.device.Client(options)
+	logging.info("IBM IoTF connected successfully, QoS Level at %i" % myQosLevel)
 	client.connect()
 	myQosLevel = 1
-	logging.info("IBM IoTF connected successfully, QoS Level at %i" % myQosLevel)
-except Exception, e:
+	client.commandCallback = myCommandCallback
+except ibmiotf.ConnectionException as e:
 	logging.debug(str(e))
 	print str(e)
 
@@ -21,3 +22,19 @@ while True:
 	myData={'txt': 'hello world', 'ts': datetime.datetime.utcnow().isoformat()+'Z'}
 	client.publishEvent("IC306A", "json", myData, myQosLevel)
 	time.sleep(5)
+
+
+
+
+def myCommandCallback(cmd):
+	print("Command received: %s" % cmd.data)
+	# if cmd.command == "setInterval":
+	# 	if 'interval' not in cmd.data:
+	# 		print("Error - command is missing required information: 'interval'")
+	# 	else:
+	# 		interval = cmd.data['interval']
+	# 	elif cmd.command == "print":
+	# 		if 'message' not in cmd.data:
+	# 			print("Error - command is missing required information: 'message'")
+	# 		else:
+	# 			print(cmd.data['message'])
